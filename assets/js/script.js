4f7c68031efdd437a98627da146d3c986b60b801
Deisel <74831846+Amelia-was@users.noreply.github.com>
2020-12-19 15:12:42 -0500

    added task name/type input

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,29 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     // prevent page reload
     event.preventDefault();
+
+    // target task name input
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    
+    // target task type input
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "New task";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    // append list info to li
+    listItemEl.appendChild(taskInfoEl);
+
+    // append to task list
     tasksToDoEl.appendChild(listItemEl);
 }
 
