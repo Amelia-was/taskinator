f829b089cc2a3a24bced8e6ab46c20f92b79c685
Deisel <74831846+Amelia-was@users.noreply.github.com>
2020-12-19 13:29:28 -0500

    added dropdown and submit event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 // variables targeting Add Task button and Task List
-var buttonEl = document.querySelector("#save-task");
-var taskListEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Create new task function
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    // prevent page reload
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "New task";
-    taskListEl.appendChild(listItemEl);
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 // When [Add Task] button is clicked, new task is appended to #tasks-to-do
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
